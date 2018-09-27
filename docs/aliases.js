5aa6a0db2801075c0e85af12653f7107004bb194
PedroGonçaloCorreia <goncalerta@gmail.com>
2018-09-27 19:37:46 +0100

    Generate documentation for GitHub Pages.

---
+++ docs/aliases.js
@@ -0,0 +1,7 @@
+var ALIASES = {};
+ALIASES['tiff'] = {};
+ALIASES['tiff'] = {};
+ALIASES['tiff'] = {};
+ALIASES['tiff'] = {};
+ALIASES['tiff'] = {};
+ALIASES['tiff'] = {};
---
+++ docs/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tiff"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"tiff/enum.Endianness.html\" title=\"enum tiff::Endianness\">Endianness</a>",synthetic:false,types:["tiff::Endianness"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
