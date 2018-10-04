166780d54ccc4400a6252f06b7a1a6010971575a
PedroGonçaloCorreia <goncalerta@gmail.com>
2018-10-04 14:48:20 +0100

    Update documentation.

--- docs/implementors/core/marker/trait.Copy.js
+++ docs/implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["tiff"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"tiff/enum.Endianness.html\" title=\"enum tiff::Endianness\">Endianness</a>",synthetic:false,types:["tiff::Endianness"]},];
+implementors["tiff_encoder"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"tiff_encoder/enum.Endianness.html\" title=\"enum tiff_encoder::Endianness\">Endianness</a>",synthetic:false,types:["tiff_encoder::Endianness"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ docs/implementors/tiff_encoder/tiff_type/trait.TiffType.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tiff_encoder"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
